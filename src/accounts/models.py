817d07ef0daa623b64753984927c669fad25fb3d
PiZissou <muedesign@me.com>
2022-04-13 17:04:49 +0200

    Créer une vue d'inscription basique

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,53 +1,7 @@
-from email.policy import default
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        create and saves a User withgiven email and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-        
-        user = self.model(
-            email = self.normalize_email(email),
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, email, password=None):
-        """
-        creates and saves a superuser with the given email and password
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-    
-
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(
-        unique=True,
-        max_length=255,
-        blank=False,
-    )
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+class CustomUser(AbstractUser):
     zip_code = models.CharField(blank=True, max_length=5)
-    
-    USERNAME_FIELD="email"
-    objects = MyUserManager()
-    
-    def has_perm(self, perm, obj=None):
-        return True
-    
-    def has_module_perms(self, app_label):
-        return True
+
+
--- src/utilisateur/urls.py
+++ src/utilisateur/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import signup
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('compte/nouveau', signup, name="signup"),
 ]

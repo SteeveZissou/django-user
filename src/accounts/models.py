d41be3dfce8acf7e222500b8b3afae3a3dd7f0d9
PiZissou <muedesign@me.com>
2022-04-13 17:25:20 +0200

    addcomment

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    '''
+    custom user class
+    '''
     zip_code = models.CharField(blank=True, max_length=5)
 
 

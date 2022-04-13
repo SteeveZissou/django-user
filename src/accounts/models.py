b33c76349505f13e686d10c231eaed071769571e
PiZissou <muedesign@me.com>
2022-04-13 16:20:39 +0200

    customUser with profile

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,53 +1,19 @@
 from email.policy import default
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from utilisateur import settings
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        create and saves a User withgiven email and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
+
+class CustomUser(AbstractUser):
+   pass
+    
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    
+def post_save_receiver(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
         
-        user = self.model(
-            email = self.normalize_email(email),
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, email, password=None):
-        """
-        creates and saves a superuser with the given email and password
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-    
-
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(
-        unique=True,
-        max_length=255,
-        blank=False,
-    )
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
-    zip_code = models.CharField(blank=True, max_length=5)
-    
-    USERNAME_FIELD="email"
-    objects = MyUserManager()
-    
-    def has_perm(self, perm, obj=None):
-        return True
-    
-    def has_module_perms(self, app_label):
-        return True
+post_save.connect(post_save_receiver, sender=settings.AUTH_USER_MODEL)
